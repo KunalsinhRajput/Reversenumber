8353ce19fba1f11cb2fe5fcd00fd753f1f4c03fb
Kunal-Rajput-18 <kunal4248rajput@gmail.com>
2024-10-28 12:25:52 +0530

    Add project files.

---
+++ Reversenumber/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reversenumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number for Reverse");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int rem;
+            int result = 0;
+
+            while (num != 0)
+            {
+                rem = num % 10;
+                result = result*10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse number is "  +  result);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+/* Console.WriteLine("Enter number for Reverse");
+            int num = Convert.ToInt32(Console.ReadLine());*/
